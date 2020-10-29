8d539aa04c40dd131e8e36764efeeacdb429e660
Erin Vaughan <erin@fishtownanalytics.com>
2020-10-29 18:13:37 +0000

    changes from office hours

--- models/staging/jaffle_shop/stg_orders.sql
+++ models/staging/jaffle_shop/stg_orders.sql
@@ -1,7 +1,33 @@
-select
+with orders as (
+    
+    select
     id as order_id,
     user_id as customer_id,
     order_date,
     status
 
 from {{ source('jaffle_shop','orders') }}
+
+),
+
+mapped_values as (
+
+    select * from {{ ref('status_mapping') }}
+
+),
+
+joined as (
+
+    select  
+        orders.*,
+        mapped_values.mapped_value,
+        case
+            when order.status in ('')
+            else 'pending'
+        end as mapped_value
+    from orders
+    left join mapped_values using (status)
+
+)
+
+select * from joined
